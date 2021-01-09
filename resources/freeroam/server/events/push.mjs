8ec6b2cf41e8d90d75c1fda62504e09ca28045a1
Nickwasused <34883496+Nickwasused@users.noreply.github.com>
2021-01-09 11:09:37 +0100

    --vue.js

--- resources/freeroam/server/events/push.mjs
+++ resources/freeroam/server/events/push.mjs
@@ -1,8 +1,27 @@
 import * as alt from 'alt';
 import * as constant from '../constants.mjs';
 
-let ipls = constant.ipls,
+let ipls,
+    blip,
+    currentDate,
+    year,
+    month,
+    date,
+    hour,
+    minute,
+    second;
+
+function resourcestart() {
+    ipls = constant.ipls;
     blip = constant.blip;
+    currentDate = new Date();
+    year = currentDate.getFullYear();
+    month = currentDate.getMonth();
+    date = currentDate.getDate();
+    hour = currentDate.getHours();
+    minute = currentDate.getMinutes();
+    second = currentDate.getSeconds();
+};
 
 function pushblips(player) {
     alt.emitClient(player, "freeroam:setupblips", (blip));
@@ -13,16 +32,16 @@ function pushipls(player) {
 };
 
 function pushdate(player) {
-    let currentDate = new Date();
-    let year = currentDate.getFullYear();
-    let month = currentDate.getMonth();
-    let date = currentDate.getDate();
-    let hour = currentDate.getHours();
-    let minute = currentDate.getMinutes();
-    let second = currentDate.getSeconds();
+    currentDate = new Date();
+    date = currentDate.getDate();
+    hour = currentDate.getHours();
+    minute = currentDate.getMinutes();
+    second = currentDate.getSeconds();
+    //alt.log(date, month, year, hour, minute, second);
     player.setDateTime(date, month, year, hour, minute, second);
 };
 
 alt.onClient("getblips", pushblips);
 alt.onClient("getipls", pushipls);
 alt.onClient("getcurrentdate", pushdate);
+alt.on('resourceStart', resourcestart);
